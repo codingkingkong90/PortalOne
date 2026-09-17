8a78d8d04dc4fcab1f8493f1106f0db784319f08
Zach Seymour <zseymour66@gmail.com>
2026-09-17 14:32:28 +1000

    started working on moderation image backend

--- app/create_group.py
+++ app/create_group.py
@@ -1,7 +1,31 @@
 from tkinter import messagebox
 import string
 import customtkinter as ctk
+from services.firestore import create_group_documents
 from data.images import eye_icon, eye_off_icon
 
 def create_group_page(parent, router):
-    pass
+#interface--------------------------------------------------------------------
+    create_card = ctk.CTkFrame(parent, width=400, height=500)
+    create_card.grid_propagate(False)
+    
+    create_card.columnconfigure(0, weight=1)
+    create_card.rowconfigure(0, weight=1)
+
+    create_input_container = ctk.CTkFrame(create_card, fg_color="transparent")
+    create_input_container.grid(row=0, column=0)
+
+#create_card------------------------------------------------------------------
+    group_name_entry = ctk.CTkEntry(create_input_container, placeholder_text="Please enter a group name", width=280)
+    group_name_entry.grid(row=0, column=0, pady=10)
+
+    group_type_entry = ctk.CTkEntry(create_input_container, placeholder_text="Please enter a group type or make a custom group type", width=280)
+    group_type_entry.grid(row=1, column=0, pady=10)
+
+    group_description_entry = ctk.CTkEntry(create_input_container, placeholder_text="Please add a group description", width=280)
+    group_description_entry.grid(row=0, column=0, pady=10)
+
+    group_logo_entry
+
+    group_create_button = ctk.CTkButton(create_input_container, text="Create Group", hover_color="light blue", command=None)
+    group_create_button.grid(row=5, column=0, pady=10)
--- data/images.py
+++ data/images.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from pathlib import Path
+from tkinter import filedialog
 import customtkinter as ctk
 
 def make_icon_white(path):
@@ -14,6 +15,58 @@ def make_icon_white(path):
                 pixels[x, y] = (255, 255, 255, a)
 
     return image
+
+def create_logo_uploader(parent):
+    selected_logo = {"path": None, "image": None}
+
+    logo_preview = ctk.CTkLabel(
+        parent,
+        text="No logo selected",
+        width=150,
+        height=150,
+    )
+
+    logo_preview.grid(pady=10)
+
+    def upload_logo():
+        file_path = filedialog.askopenfilename(
+            title="Select a Group Logo",
+            filetypes=[
+                ("Images", "*.png" "*jpg" "*jpeg"),
+                ("PNG Files", "*png"),
+                ("JPEG Files", "*jpg" "*jpeg")
+            ] 
+        )
+
+        if not file_path:
+            return
+
+        image = Image.open(file_path)
+        image.thumbnail((150, 150))
+
+        logo_image = ctk.CTkImage(
+            light_image=image,
+            dark_image=image,
+            size=image.size
+        )
+
+        logo_preview.configure(
+            image=logo_image,
+            text=""
+        )
+
+        selected_logo["path"] = file_path
+        selected_logo["image"] = file_path
+
+    upload_button = ctk.CTkButton(
+        parent,
+        text="Upload Logo",
+        command=upload_logo
+    )
+
+    upload_button.grid(pady=10)
+
+    return selected_logo
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
